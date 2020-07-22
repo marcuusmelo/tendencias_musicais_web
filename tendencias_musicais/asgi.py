dd61a5ec97feb09648c755190b73b407d748cf61
marcuusmelo <melo.mvinicius@gmail.com>
2020-07-22 20:23:35 -0300

    creating django project

---
+++ tendencias_musicais/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tendencias_musicais project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tendencias_musicais.settings')
+
+application = get_asgi_application()
