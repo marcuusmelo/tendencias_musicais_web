bf53f20629694705821497c032df2dcb9d17be44
marcuusmelo <melo.mvinicius@gmail.com>
2020-08-05 20:45:50 -0300

    add heroku to allowed hosts

--- tendencias_musicais/settings.py
+++ tendencias_musicais/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sleepy-shore-35816.herokuapp.com']
 
 
 # Application definition
